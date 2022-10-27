884bb3784702a6708e542c27cc6af6052d713fec
PaulYuthi <103030939+PaulYuthi@users.noreply.github.com>
2022-10-27 16:43:21 +0200

    Add files via upload

---
+++ src/Project/Main.scala
@@ -0,0 +1,52 @@
+package Project
+import scala.collection.mutable.ListBuffer
+import java.io.FileNotFoundException
+import scala.io.Source
+
+object Main extends App {
+
+  // Chargement du fichier de consignes
+  try {
+    val fichier = Source.fromFile("Test_files/test.txt", "utf-8")
+    val lignes: Iterator[String] = fichier.getLines()
+    val liste: List[String] = lignes.toList
+
+    // Creation de variables
+    var max_coord = liste(0).split(" ")
+    var position_initiale = ListBuffer[Array[String]]()
+    var consignes = ListBuffer[String]()
+
+    // Definir les lignes impaires comme étant les consignes de déplacements
+    // Definir les lignes paires comme étant la position et l'orientation de la tondeuse
+    for ((ligne, count) <- liste.zipWithIndex) {
+      if (count > 0 && count % 2 == 1) {
+        position_initiale += ligne.split(" ")
+      }
+      else if (count > 0 && count % 2 == 0) {
+        consignes += ligne
+      }
+    }
+
+    // Pour chaque tondeuse, creer un objet de la classe Tondeuse
+    println("Position finale des tondeuses :")
+    var i: Int = 0
+    for ((position, consigne) <- position_initiale.zip(consignes)) {
+      i += 1
+      val tondeuse = new Tondeuse(
+        (position(0).toInt, position(1).toInt),
+        position(2).charAt(0),
+        (max_coord(0).toInt, max_coord(1).toInt))
+
+      // Afficher le resultat final
+      tondeuse.run(consigne)
+      println(s"Tondeuse $i : $tondeuse")
+
+    }
+  } catch {case error: FileNotFoundException
+    => println("Le fichier n'existe pas")
+  case error: NumberFormatException
+    => println("Les coordonnées renseignées n'ont pas le bon format")
+  case error:ArrayIndexOutOfBoundsException
+  => println("Les dimensions renseignées de la pelouse sont incomplètes")
+  }
+}
---
+++ src/Project/Tondeuse.scala
@@ -0,0 +1,73 @@
+package Project
+
+// Creation de la classe Tondeuse qui traite les consignes puis renvoie les coordonnées et l'orientation des objets tondeuse
+class Tondeuse(var coord:(Int, Int),
+               var orientation:Char,
+               val max_coord:(Int, Int)){
+
+  // Definir le format de sortie des coordonnees de la tondeuse
+  override def toString: String = {
+    s"${this.coord._1} ${this.coord._2} ${this.orientation}"
+  }
+
+  // Si la tondeuse avance, definir son mouvement dans l'espace
+  def avancer: Unit = {
+    this.coord = this.orientation match {
+
+      case 'N' if this.coord._2 + 1 <= this.max_coord._2
+      => (this.coord._1, this.coord._2 + 1)
+
+      case 'E' if this.coord._1 + 1 <= this.max_coord._2
+      => (this.coord._1 + 1, this.coord._2)
+
+      case 'W' if this.coord._1 - 1 >= 0
+      => (this.coord._1 - 1, this.coord._2)
+
+      case 'S' if this.coord._2 - 1 >= 0
+      => (this.coord._1, this.coord._2 - 1)
+
+      case autre
+      => this.coord
+    }
+  }
+
+  // Definir l'orientation de la tondeuse en cas de rotation vers la gauche
+  def gauche: Unit = {
+    orientation = orientation match {
+      case 'N' => 'W'
+      case 'E' => 'N'
+      case 'W' => 'S'
+      case 'S' => 'E'
+      case autre => orientation
+    }
+  }
+
+  // Definir l'orientation de la tondeuse en cas de rotation vers la droite
+  def droite: Unit = {
+    orientation = orientation match {
+      case 'N' => 'E'
+      case 'E' => 'S'
+      case 'W' => 'N'
+      case 'S' => 'W'
+      case autre => orientation
+    }
+  }
+
+  // Appeler la fonction correspondant a la consigne indiquée dans le fichier
+  def action(consigne: Char): Unit = {
+    consigne match {
+      case 'A' => this.avancer
+      case 'G' => this.gauche
+      case 'D' => this.droite
+      case autre => orientation
+    }
+  }
+
+  // Effectuer les consignes de mouvement
+  def run(consigne: String): Unit = {
+    for (c <- consigne) {
+      this.action(c)
+    }
+  }
+
+}
